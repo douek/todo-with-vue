0327203b197e9ba4187f91b45f97f4dda8af8c9e
Daniel Douek <dadouek@gmail.com>
2020-07-28 17:04:35 +0300

    create todo with ctrl+enter
    User can click ctrl+enter when focused on the text field to submit the
    todo (in additional to the add button way)

--- src/components/createTodo.vue
+++ src/components/createTodo.vue
@@ -3,7 +3,10 @@
     <v-form>
       <v-row>
         <v-col  cols="10">
-          <v-text-field v-model="todo" :rules="[rules.required]" label="NEW Todo"></v-text-field>
+          <v-text-field v-model="todo"  @keyup.native.ctrl.enter="addNew" 
+          :rules="[rules.required]" label="NEW Tod" 
+          hint="You can submit a todo with ctrl+enter">
+          </v-text-field>
         </v-col>
         <v-col cols="2">
           <v-btn color="success" @click="addNew" class="mr-4">Add</v-btn>
